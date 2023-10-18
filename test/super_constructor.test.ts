f150400bfe9b5839fdbe8ee1dcf7e62788f1a2f9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-18 23:58:41 +0700

    super constructor test

---
+++ test/super_constructor.test.ts
@@ -0,0 +1,18 @@
+describe("Super Constructor", () => {
+    class Person {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+    }
+
+    class Employee extends Person {
+        department: string;
+
+        constructor(name: string, department: string) {
+            super(name);
+            this.department = department;
+        }
+    }
+})
