0d80dcb9d1413be055d764ee33c7aea7c4febcf2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-22 13:42:39 +0700

    type cast

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -9,12 +9,34 @@ describe('Polymorphism', () => {
 
     }
 
-    class VicePresident extends Employee {
+    class VicePresident extends Manager {
 
     }
 
     function sayHello(employee: Employee): void {
-        console.info(`Hello ${employee.name}`);
+        if (employee instanceof VicePresident) {
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else if (employee instanceof Manager) {
+            const manager = employee as Manager;
+            console.info(`Hello Manager ${manager.name}`);
+        } else {
+            console.info(`Hello employee ${employee.name}`);
+
+        }
+    }
+
+    function sayHelloWrong(employee: Employee): void {
+        if (employee instanceof Manager) {
+            const manager = employee as Manager;
+            console.info(`Hello Manager ${manager.name}`);
+        } else if (employee instanceof VicePresident) {
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else {
+            console.info(`Hello employee ${employee.name}`);
+
+        }
     }
 
     it("should support polymorphism", () => {
@@ -32,6 +54,11 @@ describe('Polymorphism', () => {
         sayHello(new Employee("Rama"));
         sayHello(new Manager("Fajar"));
         sayHello(new VicePresident("Fadhillah"));
+    })
 
+    it("should support parameter polymorphism wrong", () => {
+        sayHelloWrong(new Employee("Rama"));
+        sayHelloWrong(new Manager("Fajar"));
+        sayHelloWrong(new VicePresident("Fadhillah"));
     })
 })
