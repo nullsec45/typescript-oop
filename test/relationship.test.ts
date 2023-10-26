85826b15ca7e0a1ef88db75693652533f4b5f10d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-27 01:53:07 +0700

    relationship

---
+++ test/relationship.test.ts
@@ -0,0 +1,18 @@
+describe("relationship", () => {
+    class Person {
+        constructor(public name: string) {
+
+        }
+    }
+
+    class Customer {
+        constructor(public name: string) {
+
+        }
+    }
+
+    it("should support", () => {
+        const person: Person = new Customer("Fajar");
+        console.info(person.name);
+    })
+})
